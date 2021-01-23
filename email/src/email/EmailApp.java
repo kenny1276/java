98e4be070704be9168dc938aa6743deea8e4b32e
jurij <jurij@DESKTOP-I0U2SBA>
2021-01-23 09:16:02 +0100

    Avtomatic generate email for easy IT

---
+++ email/src/email/EmailApp.java
@@ -0,0 +1,11 @@
+package email;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		Email em1 = new Email("Kenny", "Blocker");
+		System.out.println(em1.showInfo());
+
+	}
+
+}
